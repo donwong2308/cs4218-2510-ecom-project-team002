cee761817acb363dae75133e08aef1e790b0efdc
davidni <davidni055@gmail.com>
2025-10-20 22:52:22 +0800

    fix model files in server

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,26 +8,27 @@ export default {
   // run all backend tests
   testMatch: [
     "<rootDir>/controllers/*.test.js",
-    "<rootDir>/middlewares/*.test.js", 
+    "<rootDir>/middlewares/*.test.js",
     "<rootDir>/helpers/*.test.js",
     "<rootDir>/models/*.test.js",
+    "<rootDir>/models/*.integration.test.js",
     "<rootDir>/config/*.test.js",
     "<rootDir>/controllers/__integration__/*.test.js",
     "<rootDir>/middlewares/__integration__/*.test.js",
     "<rootDir>/helpers/__integration__/*.test.js",
     "<rootDir>/models/__integration__/*.test.js",
-    "<rootDir>/config/__integration__/*.test.js"
+    "<rootDir>/config/__integration__/*.test.js",
   ],
 
   // jest code coverage
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     "controllers/**/*.js",
     "middlewares/**/*.js",
     "helpers/**/*.js",
     "models/**/*.js",
     "config/**/*.js",
-    "!**/*.test.js"
+    "!**/*.test.js",
   ],
   coverageThreshold: {
     global: {
